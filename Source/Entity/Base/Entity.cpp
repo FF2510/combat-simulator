665803e2ba83be9fc6b0668bd612c26fef68174f
FF2510 <faschina.frederic@icloud.com>
2025-04-17 21:59:34 +0200

    Entity Implementation

--- Source/Entity/Base/Entity.cpp
+++ Source/Entity/Base/Entity.cpp
@@ -1,29 +1,57 @@
 ﻿#include "Entity.h"
+#include <iostream>
+#include <stdexcept>
+#include "../Data/Stats.h"
 
 Entity::Entity(const std::string& name, Stats* stats)
 {
+    if (stats != nullptr)
+    {
+        // Assign the provided stats pointer to the internal member.
+        this->_stats = stats;
+    }
+    else
+    {
+        // Throw an error if an invalid (null) stats pointer is provided.
+        throw std::invalid_argument("Invalid stats structure assigned to entity.");
+    }
 }
 
 Entity::~Entity()
 {
-}
-
-unsigned short Entity::GetHealth() const
-{
-}
-
-unsigned short Entity::GetDamage() const
-{
-}
-
-unsigned short Entity::GetDefense() const
-{
-}
-
-unsigned short Entity::GetSpeed() const
-{
+    if (_stats != nullptr)
+    {
+        // Delete the associated Stats object and reset the pointer.
+        delete _stats; _stats = nullptr;
+    }
 }
 
 bool Entity::AreMatchingEntities(const Entity& a, const Entity& b)
 {
+    return (a.Name == b.Name) || (a._stats == b._stats);
 }
+
+
+#pragma region Getter Methods
+
+    unsigned short Entity::GetHealth() const
+    {
+        return (_stats != nullptr) ? _stats->Health : 0;
+    }
+
+    unsigned short Entity::GetDamage() const
+    {
+        return (_stats != nullptr) ? _stats->Damage : 0;
+    }
+
+    unsigned short Entity::GetDefense() const
+    {
+        return (_stats != nullptr) ? _stats->Defense : 0;
+    }
+
+    unsigned short Entity::GetSpeed() const
+    {
+        return (_stats != nullptr) ? _stats->Speed : 0;
+    }
+
+#pragma endregion
