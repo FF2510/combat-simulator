7ef8de41670195431a3e4d6961dc049ac5440dca
FF2510 <faschina.frederic@icloud.com>
2025-04-17 17:15:49 +0200

    Creator -> Method & Member Declarations

--- Source/UI/Creator/Creator.h
+++ Source/UI/Creator/Creator.h
@@ -1,6 +1,32 @@
 ﻿#pragma once
+#include <vector>
+class Combatant;
 
+/**
+ * @brief Handles the creation and registration of Combatant objects.
+ *
+ * The Creator class serves as both a factory and a registry for Combatant instances.
+ * It enables the interactive creation of new Combatant objects via user input,
+ * and it maintains an internal list of all created Combatants.
+ */
 class Creator final
 {
+    public:
+        
+        // Destructor that cleans up all registered Combatant objects.
+        ~Creator();
+
+        // Starts the creation process of a new Combatant.
+        static Combatant& CreateNewCombatant(Creator& creator);
+
+        // Force-deletes all registered Combatant objects.
+        void ResetRegister();
+
+    private:
+
+        // Registers a new Combatant.
+        void RegisterCombatant(Combatant* combatantPtr);
     
+        // Container for all registered Combatant pointers.
+        std::vector<Combatant*> _combatants;
 };
