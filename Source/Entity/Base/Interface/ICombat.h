1d259e788ad51612e7cdb4d7e7da1aba7c37ec0f
FF2510 <faschina.frederic@icloud.com>
2025-04-17 17:51:27 +0200

    Create ICombat Interface

---
+++ Source/Entity/Base/Interface/ICombat.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+class Combatant;
+
+
+/**
+ * @brief Interface for basic combat logic.
+ *
+ * This interface defines the essential methods required for combat operations.
+ * Classes implementing this interface should provide concrete logic for
+ * executing attacks and handling incoming damage.
+ */
+class ICombat
+{
+public:
+    
+    virtual ~ICombat() = default;
+        
+    // Executes an attack on a specified combatant.
+    virtual void Attack(Combatant& target) = 0;
+
+    // Handles damage inflicted on the combatant.
+    virtual void OnDamage(unsigned short amount) = 0;
+};
