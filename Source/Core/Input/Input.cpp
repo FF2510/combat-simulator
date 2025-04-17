d4977390036f9ce13966f57c6989be167126bd02
FF2510 <faschina.frederic@icloud.com>
2025-04-17 19:05:28 +0200

    Input -> Declarations & Implementations (Empty)

--- Source/Core/Input/Input.cpp
+++ Source/Core/Input/Input.cpp
@@ -1,1 +1,22 @@
 ﻿#include "Input.h"
+
+
+template <typename Type> Type Input::GetInputAs(const std::string prompt)
+{
+    
+}
+
+template <> std::string Input::GetInputAs<std::string>(const std::string prompt)
+{
+    
+}
+
+template <> unsigned short Input::GetInputAs<unsigned short>(const std::string prompt)
+{
+    
+}
+
+template <> bool Input::GetInputAs<bool>(const std::string prompt)
+{
+    
+}
