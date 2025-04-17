dc7f04d1fe052a178775c78abbbd941aba142e88
FF2510 <faschina.frederic@icloud.com>
2025-04-17 17:44:43 +0200

    Creator -> Method Implementations (Empty)

--- Source/UI/Creator/Creator.cpp
+++ Source/UI/Creator/Creator.cpp
@@ -1,1 +1,20 @@
 ﻿#include "Creator.h"
+
+
+
+Creator::~Creator()
+{
+    ResetRegister();
+}
+
+Combatant& Creator::CreateNewCombatant(Creator& creator)
+{
+}
+
+void Creator::ResetRegister()
+{
+}
+
+void Creator::RegisterCombatant(Combatant* combatantPtr)
+{
+}
