e7861fe95f0e3b3d104a0003b8c6284810117f88
FF2510 <faschina.frederic@icloud.com>
2025-04-17 19:16:45 +0200

    Loop -> Declarations & Implementations (Empty)

--- Source/Core/Loop/Loop.h
+++ Source/Core/Loop/Loop.h
@@ -1,6 +1,15 @@
 ﻿#pragma once
+class Combatant;
 
-class Loop
+/**
+ * @brief Manages the main game loop.
+ *
+ * This class controls the flow of a battle between two combatants,
+ * managing turn order, updating combat rounds, and displaying
+ * combat information until a winner is determined.
+ */
+class Loop final
 {
-    
+    // Starts the game loop for a battle between two combatants.
+    void Start(Combatant& combatantA, Combatant& combatantB);
 };
