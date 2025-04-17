0f65c80af00d115c7b3a9585351161db2b263cc5
FF2510 <faschina.frederic@icloud.com>
2025-04-17 18:42:07 +0200

    Combatant -> Method Declarations & Implementations (Empty)

--- Source/Entity/Combatant.cpp
+++ Source/Entity/Combatant.cpp
@@ -1,1 +1,33 @@
 ﻿#include "Combatant.h"
+#include "Data/Stats.h"
+
+Combatant::Combatant(const std::string& name, unsigned short health, unsigned short defense, unsigned short damage, unsigned short speed)
+    : Entity(name, new Stats(health, damage, defense, speed))
+{
+    // Additional construction logic:
+    // ...
+}
+
+Combatant::~Combatant()
+{
+    
+}
+
+bool Combatant::bIsAlive() const
+{
+    
+}
+
+#pragma region Interface Implementation
+
+    void Combatant::Attack(Combatant& target)
+    {
+    
+    }
+
+    void Combatant::OnDamage(unsigned short amount)
+    {
+    
+    }
+
+#pragma endregion 
--- Source/Entity/Combatant.h
+++ Source/Entity/Combatant.h
@@ -1,6 +1,30 @@
 ﻿#pragma once
+#include "Base/Entity.h"
+#include "Base/Interface/ICombat.h"
+#include <string>
+struct Stats;
 
-class Combatant
+
+class Combatant final : public Entity, public ICombat
 {
+    public:
     
+        // Constructs a new Combatant object.
+        Combatant(const std::string& name, unsigned short health, unsigned short defense, unsigned short damage, unsigned short speed);
+
+        // Virtual destructor.
+        ~Combatant() override;
+    
+
+        // Executes an attack on another combatant.
+        void Attack(Combatant& target) override;
+
+        // Checks whether the combatant is still alive.
+        bool bIsAlive() const;
+
+
+    protected:
+        
+        // Reacts to incoming damage.
+        void OnDamage(unsigned short amount) override;
 };
